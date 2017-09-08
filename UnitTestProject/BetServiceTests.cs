8c89d771c9b214d879ca8203f2b8bca8738dbd06
kristina.p.georgieva <kristina.p.georgieva@gmail.com>
2017-09-08 16:50:13 +0300

    Wrote the BetService unit tests.

---
+++ UnitTestProject/BetServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieScrapper.Business;
+using MovieScrapper.Data.Interfaces;
+using MovieScrapper.Entities;
+using Rhino.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class BetServiceTests
+    {
+        [TestMethod]
+        public void GetAllUserBets_Should_BeCalledOnce_WhenTheCorrectRepositoryIsPassed()
+        {
+            var betRepositoryMock = MockRepository.GenerateMock<IBetRepository>();
+
+            //Arrange
+            betRepositoryMock.Expect(dao => dao.GetAllUserBets(Arg<string>.Is.Anything)).Return(Arg<IEnumerable<Bet>>.Is.Anything).Repeat.Once(); ;
+
+            var betService = new BetService(betRepositoryMock);
+
+            //Act
+            betService.GetAllUserBets("1");
+
+            //Assert
+            betRepositoryMock.VerifyAllExpectations();
+        }
+
+        [TestMethod]
+        public void GetUserBetEntity_Should_BeCalledOnce_WhenTheCorrectRepositoryIsPassed()
+        {
+            var betRepositoryMock = MockRepository.GenerateMock<IBetRepository>();
+
+            //Arrange
+            betRepositoryMock.Expect(dao => dao.GetUserBetEntity(Arg<string>.Is.Anything)).Return(Arg<Bet>.Is.Anything).Repeat.Once(); ;
+
+            var betService = new BetService(betRepositoryMock);
+
+            //Act
+            betService.GetUserBetEntity("1");
+
+            //Assert
+            betRepositoryMock.VerifyAllExpectations();
+        }
+
+        [TestMethod]
+        public void MakeBetEntity_Should_BeCalledOnce_WhenTheCorrectRepositoryIsPassed()
+        {
+            var betRepositoryMock = MockRepository.GenerateMock<IBetRepository>();
+
+            //Arrange
+            betRepositoryMock.Expect(dao => dao.MakeBetEntity(Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything)).Return(Arg<Bet>.Is.Anything).Repeat.Once(); ;
+
+            var betService = new BetService(betRepositoryMock);
+
+            //Act
+            betService.MakeBetEntity("1", 1 , 1);
+
+            //Assert
+            betRepositoryMock.VerifyAllExpectations();
+        }
+
+    }
+}

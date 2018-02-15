58ac6fce89cd5bb5a3fcaacfb30e837abf461cf0
kostovd <kostov009@gmail.com>
2018-02-16 00:26:27 +0200

    Home menu added

--- MovieScrapper.Web/Site.Master.designer.cs
+++ MovieScrapper.Web/Site.Master.designer.cs
@@ -31,6 +31,15 @@ namespace MovieScrapper {
         protected global::System.Web.UI.HtmlControls.HtmlAnchor Admin;
         
         /// <summary>
+        /// Home control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor Home;
+        
+        /// <summary>
         /// Statistics control.
         /// </summary>
         /// <remarks>

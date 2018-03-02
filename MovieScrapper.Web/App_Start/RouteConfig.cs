e1fb52e811debc4e0d96743e7eb8360baabe62cd
kostovd <kostov009@gmail.com>
2018-03-03 01:31:48 +0200

    Disable Mobile View Switcher

--- MovieScrapper.Web/App_Start/RouteConfig.cs
+++ MovieScrapper.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MovieScrapper
         {
             var settings = new FriendlyUrlSettings();
             settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
+            routes.EnableFriendlyUrls(settings, new MovieScrapperFriendlyUrlResolver());
         }
     }
 }

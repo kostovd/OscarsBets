f4fbd68ada0608ae6bd101b116385b43e3bc87c9
y.atanasova <y.atanasova@PC-1272>
2020-01-31 15:51:22 +0200

    Add filter types

---
+++ MovieScrapper.Business/Enums/FilterTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieScrapper.Business.Enums
+{
+    public enum FilterTypes
+    {
+        ByName = 0,
+        ByNominations = 1,
+        ByProxiadPopularity =2
+    }
+}
